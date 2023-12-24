3f2be726d173750def901f5fcb095cfc0faf753e
akira <zup9su@virginia.edu>
2023-12-24 18:34:32 +0900

    added in record class

---
+++ model.py
@@ -0,0 +1,7 @@
+# create record class
+# way to input diff categories
+# way to visualize
+# way to track server job
+# create $ per hour
+# way to show best days to work
+# spend vs profit
---
+++ record.py
@@ -0,0 +1,12 @@
+import datetime as dt
+
+class Records:
+
+    def __init__(self, name, category, price):
+        self.name = name
+        self.category = category
+        self.price = price
+        self.dt = dt.datetime.now()
+
+    def __repr__(self):
+        return f"{self.name} is a {self.category}: ${self.price}"
