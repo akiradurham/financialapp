ae0d07c5868441bb0978755d46978042643809bd
akira <zup9su@virginia.edu>
2023-12-25 23:11:48 +0900

    formatting to put vbox on top rightside

--- viewport.py
+++ viewport.py
@@ -11,45 +11,53 @@ class Finance(QWidget):
 
         self.label = QLabel('', self)
         self.label.setAlignment(Qt.AlignCenter)
-        self.label.setGeometry(1080, 500, 150, 50)
         font = self.label.font()
         font.setPointSize(11)
         self.label.setFont(font)
 
         self.namebox = QLineEdit('Enter item name', self)
-        self.namebox.setGeometry(1080, 450, 150, 50)
         self.namebox.setFont(font)
 
-
+        self.hbox = QHBoxLayout()
+        self.category1 = QRadioButton('Revenue')
+        self.category2 = QRadioButton('Expense')
+        self.hbox.addWidget(self.category1)
+        self.hbox.addWidget(self.category2)
 
         self.pricebox = QLineEdit('Enter price value', self)
-        self.pricebox.setGeometry(1080, 310, 150, 50)
         self.pricebox.setFont(font)
 
         self.a_button = QPushButton('Add a Record', self)
-        self.a_button.setGeometry(1080, 550, 150, 50)
         self.a_button.setFont(font)
         self.a_button.clicked.connect(self.add)
 
         self.d_button = QPushButton('Delete a Record', self)
-        self.d_button.setGeometry(1080, 620, 150, 50)
         self.d_button.setFont(font)
         self.d_button.clicked.connect(self.delete)
 
-        # self.rightbox = QVBoxLayout(QWidget())
-        # self.rightbox.addWidget(self.label)
-        # self.rightbox.addWidget(self.namebox)
-        # self.rightbox.addWidget(self.pricebox)
-        # self.rightbox.addWidget(self.a_button)
-        # self.rightbox.addWidget(self.d_button)
-        # self.rightbox.setAlignment(Qt.AlignTop and Qt.AlignRight)
-        # self.rightbox.setFixedWidth(150)
+        self.rightbox = QVBoxLayout()
+        self.rightbox.addWidget(self.namebox)
+        self.rightbox.addLayout(self.hbox)
+        self.rightbox.addWidget(self.pricebox)
+        self.rightbox.addWidget(self.label)
+        self.rightbox.addWidget(self.a_button)
+        self.rightbox.addWidget(self.d_button)
+        self.rightbox.setAlignment(Qt.AlignTop)
+        self.rightbox.setSpacing(30)
+
+        self.width = QWidget()
+        self.width.setMaximumWidth(200)
+        self.width.setLayout(self.rightbox)
+
+        self.mainlayout = QHBoxLayout(self)
+        self.mainlayout.addWidget(self.width, alignment=Qt.AlignRight)
+
+        self.setLayout(self.rightbox)
 
         self.setWindowIcon(QIcon('financelogo.png'))
         self.setWindowTitle('Financial Tracking Application')
         self.setFixedSize(1280, 720)
         self.move(325, 150)
-        self.show()
 
     def add(self):
 
@@ -71,4 +79,5 @@ ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(process)
 
 app = QApplication([])
 scene = Finance()
+scene.show()
 app.exec()
