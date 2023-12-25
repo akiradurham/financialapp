f9b1248c7154b95052fd2fd765738842a3f2dff9
akira <zup9su@virginia.edu>
2023-12-25 15:24:20 +0900

    adding in logo and taskbar icons, slight build of gui, setting up view

--- viewport.py
+++ viewport.py
@@ -1,14 +1,31 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 import model as m
+import ctypes
 
-window = QApplication([])
-scene = QWidget()
-layout = QVBoxLayout()
 
-layout.addWidget(QRadioButton('Option 1'))
-layout.addWidget(QRadioButton('Option 2'))
+def addCourse():
 
-scene.setLayout(layout)
-scene.show()
 
-window.exec()
+def removeCourse():
+
+
+class Finance(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowIcon(QIcon('financelogo.png'))
+        self.setWindowTitle('Financial Tracking Application')
+        self.setGeometry(10, 10, 1280, 720)
+        self.move(850, 300)
+        self.show()
+
+
+appId = 'financial.app.allowing.taskbar.customization'
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appId)
+#  adding taskbar icon from https://stackoverflow.com/questions/1551605/how-to-set-applications-taskbar-icon-in-windows-7/1552105#1552105
+
+app = QApplication([])
+scene = Finance()
+app.exec()
