7868dcc4b4e0640420ab2afba083887dd9bb0337
akira <zup9su@virginia.edu>
2023-12-25 15:36:16 +0900

    adding visual buttons

--- viewport.py
+++ viewport.py
@@ -5,27 +5,25 @@ import model as m
 import ctypes
 
 
-def addCourse():
+def addRecord():
 
 
-def removeCourse():
-
-
-class Finance(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowIcon(QIcon('financelogo.png'))
-        self.setWindowTitle('Financial Tracking Application')
-        self.setGeometry(10, 10, 1280, 720)
-        self.move(850, 300)
-        self.show()
+def deleteRecord():
 
 
 appId = 'financial.app.allowing.taskbar.customization'
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appId)
 #  adding taskbar icon from https://stackoverflow.com/questions/1551605/how-to-set-applications-taskbar-icon-in-windows-7/1552105#1552105
-
+#  isolates the python script from the IDE, "own function" so we can give separate logo that is not python exe
 app = QApplication([])
-scene = Finance()
+app.setWindowIcon(QIcon('financiallogo.png'))
+window = QMainWindow()
+
+window.setWindowTitle('Finance Tracker Application')
+window.addButton = QPushButton('Add Record')
+window.deleteButton = QPushButton('Delete Record')
+window.addButton.clicked(addRecord())
+window.deleteButton.clicked(deleteRecord())
+window.show()
+
 app.exec()
